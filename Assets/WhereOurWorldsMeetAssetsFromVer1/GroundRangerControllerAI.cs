e591fdf83c4b723de9e8c1576b6eea187c79b4ed
Pval <lujanschiiandrei@gmail.com>
2026-09-19 16:21:04 +0100

    Ranger patrols and chases

--- Assets/WhereOurWorldsMeetAssetsFromVer1/GroundRangerControllerAI.cs
+++ Assets/WhereOurWorldsMeetAssetsFromVer1/GroundRangerControllerAI.cs
@@ -1,10 +1,11 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class GroundRangerControllerAI : MonoBehaviour
 {
     NavMeshAgent nma;
-    [SerializeField] float moveSpeed;
     [SerializeField] float attackCooldown;
     [SerializeField] float sightRange;
     GameObject[] players;
@@ -12,6 +13,8 @@ public class GroundRangerControllerAI : MonoBehaviour
     GameObject target;
     EnemyHealthComponent ehc;
     Vector3 finalPosition;
+    bool pathFinding = false;
+    [SerializeField] float walkingCooldown;
 
     [SerializeField] float walkRadius;
 
@@ -35,14 +38,14 @@ public class GroundRangerControllerAI : MonoBehaviour
                 if (distance < smallestDistance)
                 {
                     smallestDistance = distance;
-                    if (distance <= sightRange && ehc.health > 0)
+                    if (distance > sightRange && ehc.health > 0)
                         target = players[i];
                     else
                         target = gameObject;
                 }
             }
         }
-        else if (Vector3.Distance(players[0].transform.position, transform.position) <= sightRange && ehc.health > 0)
+        else if (Vector3.Distance(players[0].transform.position, transform.position) > sightRange && ehc.health > 0)
         {
             target = players[0];
         }
@@ -50,13 +53,32 @@ public class GroundRangerControllerAI : MonoBehaviour
         {
             target = gameObject;
         }
-        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
-        randomDirection += transform.position;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
-        Vector3 finalPosition = hit.position;
-        nma.destination = finalPosition;
+
+        // Starts maneuvering like snake my solid
+        if (!pathFinding && Vector3.Distance(target.transform.position, transform.position) <= sightRange)
+        {
+            StartCoroutine("RangerWalking");
+
+        }
+        if (target != null && !pathFinding)
+        {    
+            nma.destination = target.transform.position;
+        }
 
         //get a point around the currently targeted player, without needing to run around the player in order to pathfind, and make sure it is still an available navmesh. point, shoot, pause, pathfind again.
+    }
+
+    IEnumerator RangerWalking()
+    {
+        Debug.Log("Works");
+        pathFinding = true;
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius; // 'Random.insideUnitSphere' generates a random vector3, and multiplies this with the walk radius to see how far it can go.
+        randomDirection += transform.position; // Stupid way of walking, I wont fix this.
+        NavMeshHit hit; 
+        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, NavMesh.AllAreas); 
+        Vector3 finalPosition = hit.position;
+        nma.destination = finalPosition;
+        yield return new WaitForSeconds(walkingCooldown);
+        pathFinding = false;
     }
 }
